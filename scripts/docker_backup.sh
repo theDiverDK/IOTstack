cdfbfce87832acbe4f2adba5988cd4effff3364e
Søren Reinke <soren@reinke.dk>
2020-09-09 09:53:50 +0200

    Fixed dropbox backup to work

--- scripts/docker_backup.sh
+++ scripts/docker_backup.sh
@@ -68,7 +68,7 @@ if [ -f ./backups/dropbox ]; then
 	#list older files to be deleted from cloud (exludes last 7)
 	#to change dropbox backups retained, change below -7 to whatever you want
 	echo "checking for old backups on dropbox"
-	files=$($dropboxuploader list $dropboxfolder | awk {' print $3 '} | tail -n +2 | head -n -7)
+	files=$($dropboxuploader list | awk {' print $3 '} | tail -n +2 | head -n -7)
 
 	#write files to be deleted to dropbox logfile
 	sudo touch $dropboxlog
@@ -83,7 +83,7 @@ if [ -f ./backups/dropbox ]; then
 		input=$dropboxlog
 		while IFS= read -r file
 		do
-	    	$dropboxuploader delete $dropboxfolder/$file
+	    	$dropboxuploader delete $file
 		done < "$input"
 	fi
 	echo "backups deleted from dropbox" >>$dropboxlog
